7ee0565aacf9938615c684b7463698396fba3043
Jason Warner <jcwarner@gmail.com>
2018-04-07 19:22:39 -0500

    getTweets added  to find last 20 tweets from specified user

--- liri.js
+++ liri.js
@@ -14,12 +14,12 @@ const client = new Twitter(keys.twitter);
 let args = [];
 
 // store user arguments in args array
-for (let i = 2; i <process.argv.length; i++) {
+for (let i = 2; i < process.argv.length; i++) {
     args.push(process.argv[i]);
 }
 
 // switch statement to run proper action block
-switch(args[0]) {
+switch (args[0]) {
     case "my-tweets":
         getTweets();
         break;
@@ -43,6 +43,31 @@ switch(args[0]) {
 
 // function to display 20 most recent user tweets
 function getTweets() {
+    var params = { screen_name: args[1] };
+
+    console.log("Tweets from " + args[1] + ":");
+    console.log("");
+
+    client.get('statuses/user_timeline', params, function (error, tweets, response) {
+
+        if (!error) {
+            //for (let j = 0; j < tweets.length; j++)
+            //console.log(JSON.stringify(tweets, null, 2));
+
+            if (tweets.length < 20) {
+                for (let j = 0; j < tweets.length; j++) {
+                    console.log(tweets[j].created_at + ": " + tweets[j].text);
+                }
+            } else {
+                for (let j = 0; j < 20; j++) {
+                    console.log(tweets[j].created_at + ": " + tweets[j].text);
+                }
+            }
+
+        } else {
+            console.log("Code " + error[0].code + " - " + error[0].message);
+        }
+    });
 
 }
 
@@ -58,5 +83,5 @@ function getMovie() {
 
 // function to display action from random.txt
 function getAction() {
-    
+
 }
