42cc78e73a5cf3125410fb760feb10dbed532644
Jason Warner <jcwarner@gmail.com>
2018-04-07 19:32:17 -0500

    modified getTweet function  by adding search parameters to params and removing unnecessary if-statements in cliet.get

--- README.md
+++ README.md
@@ -150,6 +150,7 @@ require("dotenv").config();
 
 ### What Each Command Should Do
 
+# DONE
 1. `node liri.js my-tweets`
 
    * This will show your last 20 tweets and when they were created at in your terminal/bash window.
--- liri.js
+++ liri.js
@@ -43,7 +43,13 @@ switch (args[0]) {
 
 // function to display 20 most recent user tweets
 function getTweets() {
-    var params = { screen_name: args[1] };
+    var params = {
+        screen_name: args[1],
+        count: 20,
+        result_type: "recent",
+        lang: "en"
+
+    };
 
     console.log("Tweets from " + args[1] + ":");
     console.log("");
@@ -51,19 +57,9 @@ function getTweets() {
     client.get('statuses/user_timeline', params, function (error, tweets, response) {
 
         if (!error) {
-            //for (let j = 0; j < tweets.length; j++)
-            //console.log(JSON.stringify(tweets, null, 2));
-
-            if (tweets.length < 20) {
-                for (let j = 0; j < tweets.length; j++) {
-                    console.log(tweets[j].created_at + ": " + tweets[j].text);
-                }
-            } else {
-                for (let j = 0; j < 20; j++) {
-                    console.log(tweets[j].created_at + ": " + tweets[j].text);
-                }
+            for (let j = 0; j < tweets.length; j++) {
+                console.log(tweets[j].created_at + ": " + tweets[j].text);
             }
-
         } else {
             console.log("Code " + error[0].code + " - " + error[0].message);
         }
