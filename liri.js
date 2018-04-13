f363569cbd211ca857de884d4dac89c383184bd3
Jason Warner <jcwarner@gmail.com>
2018-04-12 19:28:51 -0500

    touched up liri.js, added code comments, touched up README.md

--- liri.js
+++ liri.js
@@ -12,6 +12,7 @@ const keys = require("./liri-api-keys/keys");
 const spotify = new Spotify(keys.spotify);
 const client = new Twitter(keys.twitter);
 
+// storage array for LIRI command line arguments
 let args = [];
 
 // store user arguments in args array
@@ -40,12 +41,12 @@ function doThisAction() {
             break;
 
         default:
-            console.log("User selected action not recognized.");
+            console.log("User action not recognized.");
 
     }
 }
 
-// function to display 20 most recent user tweets
+// function to display 20 most recent user tweets from Twitter
 function getTweets() {
     let params = {
         screen_name: args[1],
@@ -55,6 +56,8 @@ function getTweets() {
 
     };
 
+    // if no username provided, then default to twitter user @nodejs
+    // else use twitter username provided
     if (args[1] === undefined) {
 
         params.screen_name = "nodejs";
@@ -62,8 +65,10 @@ function getTweets() {
         console.log(`Tweets from ${params.screen_name}:`);
         console.log(``);
 
-        client.get('statuses/user_timeline', params, function (error, tweets, response) {
+        // get tweets based on assigned params
+        client.get("statuses/user_timeline", params, function (error, tweets, response) {
 
+            // display most recent user tweets up to 20
             if (!error) {
                 for (let j = 0; j < tweets.length; j++) {
                     console.log(`${tweets[j].created_at}: ${tweets[j].text}`);
@@ -77,8 +82,10 @@ function getTweets() {
         console.log(`Tweets from ${args[1]}:`);
         console.log(``);
 
+        // get tweets based on assigned params
         client.get('statuses/user_timeline', params, function (error, tweets, response) {
 
+            // display most recent user tweets up to 20
             if (!error) {
                 for (let j = 0; j < tweets.length; j++) {
                     console.log(`${tweets[j].created_at}: ${tweets[j].text}`);
@@ -91,19 +98,18 @@ function getTweets() {
 
 }
 
-// function to display song information
+// function to display song information from Spotify
 function getSong() {
 
+    // if no song provided, default to "The Sign" by Ace of Base
+    // else search Spotify for the requested song information
     if (args[1] === undefined) {
+        // request to specific "The Sign" by Ace of Base API URL
         spotify
             .request("https://api.spotify.com/v1/tracks/0hrBpAOgrt8RXigk83LLNE")
             .then(function (data) {
-                console.log("Must be The Sign, cuz you didn't search for anything...");
-                console.log("");
-                console.log("Artist(s): " + data.artists[0].name);
-                console.log("Song Name: " + data.name);
-                console.log("Song Link: " + data.external_urls.spotify);
-                console.log("Song Album: " + data.album.name);
+                console.log(`Must be The Sign, cuz you didn't search for anything...`);
+                console.log(`\nArtist(s): ${data.artists[0].name} \nSong Name: ${data.name} \nSong Link: ${data.external_urls.spotify} \nSong Album: ${data.album.name}`);
             })
             .catch(function (err) {
                 console.error('Error occurred: ' + err);
@@ -111,43 +117,36 @@ function getSong() {
     } else {
         spotify.search({ type: "track", query: args[1] }, function (err, data) {
             if (err) {
-                console.log('Error occurred: ' + err);
+                console.log(`Error occurred: ${err}`);
             } else {
                 let songInfo = data.tracks.items[0];
 
-                console.log("Your search for " + args[1] + " returned the following song...");
-                console.log("");
-                console.log("Artist(s): " + songInfo.artists[0].name);
-                console.log("Song Name: " + songInfo.name);
-                console.log("Song Link: " + songInfo.external_urls.spotify);
-                console.log("Song Album: " + songInfo.album.name);
+                console.log(`Your search for ${args[1]} returned the following song...`);
+                console.log(`\nArtist(s): ${songInfo.artists[0].name} \nSong Name: ${songInfo.name} \nSong Link: ${songInfo.external_urls.spotify} \nSong Album: ${songInfo.album.name}`);
             }
         });
     }
-
 }
 
-// function to display movie information
+// function to display movie information from OMDB
 function getMovie() {
 
+    // if no movie provided, default to "Mr Nobody"
+    // else search OMDB with the user provided movie
     if (args[1] === undefined) {
         request("http://www.omdbapi.com/?t=mr+nobody&y=&plot=short&apikey=trilogy", function (error, response, body) {
 
-            // If the request is successful (i.e. if the response status code is 200)
             if (!error && response.statusCode === 200) {
 
-                // Parse the body of the site and recover just the imdbRating
-                // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
                 console.log(`Movie Title: ${JSON.parse(body).Title} \nMovie Year: ${JSON.parse(body).Year} \nIMDB Rating: ${JSON.parse(body).Ratings[0].Value} \nRotten Tomatoes Rating: ${JSON.parse(body).Ratings[1].Value} \nCountry: ${JSON.parse(body).Country} \nLanguage: ${JSON.parse(body).Language} \nPlot: ${JSON.parse(body).Plot} \nActors: ${JSON.parse(body).Actors}`);
             }
         });
     } else {
-
+        // split and join the movie argument with + for API URL request semantics
         let myMovie = args[1].split(" ").join("+");
 
         request("http://www.omdbapi.com/?t=" + myMovie + "&y=&plot=short&apikey=trilogy", function (error, response, body) {
 
-            // If the request is successful (i.e. if the response status code is 200)
             if (!error && response.statusCode === 200) {
 
                 console.log(JSON.parse(body));
@@ -164,20 +163,21 @@ function getAction() {
 
     fs.readFile("random.txt", "utf8", function (error, data) {
 
-        // If the code experiences any errors it will log the error to the console.
         if (error) {
             return console.log(error);
         }
 
-        // Then split it by commas (to make it more readable)
+        // split the random.tx data string into a array of strings at the comma break
         let dataArr = data.split(",");
 
         args[0] = dataArr[0];
         args[1] = dataArr[1].replace(/"/g, "");
 
+        // call doThisAction to find which action block to take
         doThisAction();
 
     });
 }
 
+// call doThisAction after command line arguments received and determine which action block to take
 doThisAction();
